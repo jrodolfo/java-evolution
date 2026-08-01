32647c3d615803801a91fd803a1d0a7bc4d9bb88
Rod Oliveira <jrodolfo@gmail.com>
2026-08-01 12:54:56 -0300

    initial spring boot project

---
+++ src/main/java/net/jrodolfo/java_evolution/JavaEvolutionApplication.java
@@ -0,0 +1,13 @@
+package net.jrodolfo.java_evolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEvolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEvolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jrodolfo/java_evolution/JavaEvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jrodolfo.java_evolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEvolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
