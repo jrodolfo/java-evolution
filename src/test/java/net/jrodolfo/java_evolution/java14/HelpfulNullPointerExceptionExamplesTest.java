0effb950f2b5ab89ec8ea3dc080cec4411cf2208
Rod Oliveira <jrodolfo@gmail.com>
2026-08-20 14:05:00 -0300

    test: clarify helpful nullpointer assertion

--- src/test/java/net/jrodolfo/java_evolution/java14/HelpfulNullPointerExceptionExamplesTest.java
+++ src/test/java/net/jrodolfo/java_evolution/java14/HelpfulNullPointerExceptionExamplesTest.java
@@ -1,6 +1,7 @@
 package net.jrodolfo.java_evolution.java14;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.Test;
@@ -11,14 +12,12 @@ class HelpfulNullPointerExceptionExamplesTest {
 
 	@Test
 	void nullPointerExceptionMessageIdentifiesTheNullPartOfTheChain() {
-		Throwable failure = org.assertj.core.api.Assertions.catchThrowable(examples::triggerHelpfulNullPointerException);
+		Throwable failure = catchThrowable(examples::triggerHelpfulNullPointerException);
 
 		// Helpful NPE details can be disabled with -XX:-ShowCodeDetailsInExceptionMessages.
 		assumeTrue(failure.getMessage() != null, "helpful NullPointerException details are disabled in this JVM");
 
-		assertThatThrownBy(() -> {
-			throw failure;
-		})
+		assertThat(failure)
 				.as("Java 14 helpful NullPointerException messages should identify the null expression")
 				.isInstanceOf(NullPointerException.class)
 				.hasMessageContaining("address()");
